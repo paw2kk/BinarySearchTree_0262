05e18917c2d1b58ff9292c0fe5d38234bf082dee
paw2kk <wndxakk@gmail.com>
2025-06-26 10:08:33 +0700

    membuat anak dari root kanan maupun kiri ke null

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -36,6 +36,12 @@ void insert(){
 
     //Step 2 : Assign value to the data field of the new node
     newNode->info = x;
+
+    //Step 3 : Make the left and right child of the new node point to NULL
+    newNode->leftchild = nullptr;
+    newNode->rightchild = nullptr;
+
+    
 }
 
 }
