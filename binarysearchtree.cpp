983ddf0379ca50123dbb78e904634e5c92da04ee
paw2kk <wndxakk@gmail.com>
2025-06-26 10:18:44 +0700

    jika jumlah value yang dimasukkan di dalam data lebih kecil dari parentnya maka left child akan menjadi point new node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -55,6 +55,15 @@ void insert(){
         //5b : Exit
         return;
     }
+
+    //Step 6 : If the value in the data field of new node is less than that of parent
+    if (x < parent->info){
+        //6a : Make the left child of parent point to the new node
+        parent->leftchild = newNode;
+
+        //6b : Exit
+        return;
+    }
 }
 
 }
