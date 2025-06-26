a1c17e125f2c6b23f4421a055f738a9976b59c06
paw2kk <wndxakk@gmail.com>
2025-06-26 10:22:17 +0700

    jika value yang akan dimasukkan lebih besar maka sisi kanan dari point akan menjadi new node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -64,6 +64,15 @@ void insert(){
         //6b : Exit
         return;
     }
+
+    //Step 7 : If the value in the data field of the new node is greater than that of the parent
+    else if (x > parent->info){
+        //7a : Make the right child of parent point to the new node
+        parent->rightchild = newNode;
+
+        //7b : Exit
+        return;
+    }
 }
 
 }
